a326aa33945854111bd9218f8c555d32fe512427
jswiss <swissjoshua@gmail.com>
2020-12-22 23:56:24 +0000

    add basic md setup

---
+++ src/posts/index.ts
@@ -0,0 +1,26 @@
+//@ts-ignore
+import allPosts from './*.md'
+
+interface Metadata {
+  layout: string;
+  title: string;
+  summary?: string;
+  date: string;
+  tags: string;
+
+}
+interface Post {
+  html: string;
+  metadata: Metadata;
+  filename: string;
+  date?: number;
+}
+
+export const posts = allPosts.map(transformPost)
+  .sort((a: Post, b: Post) => b.date - a.date);
+
+function transformPost({html, metadata, filename}: Post) {
+  const slug = filename.replace(/.md$/, '');
+  const date = new Date(metadata.date);
+  return {...metadata, html, slug, filename, date};
+}
